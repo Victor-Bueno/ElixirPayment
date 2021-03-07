8d95e57eb7e2dc8aa226c1abb74ada66fc687ef2
Victor <victor.bueno0720@gmail.com>
2021-03-07 11:22:41 -0300

    Improved error view

--- elixirpay/lib/elixirpay_web/views/error_view.ex
+++ elixirpay/lib/elixirpay_web/views/error_view.ex
@@ -1,5 +1,6 @@
 defmodule ElixirpayWeb.ErrorView do
   use ElixirpayWeb, :view
+  alias Ecto.Changeset
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
@@ -12,5 +13,17 @@ defmodule ElixirpayWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
