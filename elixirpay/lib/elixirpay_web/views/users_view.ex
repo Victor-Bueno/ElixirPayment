7be36aabca44899bfb91eae1f86f867bc42ccb37
Victor <victor.bueno0720@gmail.com>
2021-03-07 12:13:31 -0300

    Showing the created account on user view

--- elixirpay/lib/elixirpay_web/views/users_view.ex
+++ elixirpay/lib/elixirpay_web/views/users_view.ex
@@ -1,12 +1,16 @@
 defmodule ElixirpayWeb.UsersView do
-  alias Elixirpay.User
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  alias Elixirpay.{Account, User}
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname, account: %Account{id: account_id, balance: balance}}}) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
